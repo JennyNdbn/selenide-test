2e583288649109e243daf0c49e0537bf00a2b72f
JennyNdbn <NadobnayaEA@yandex.ru>
2022-12-21 00:10:16 +0700

    added GitHubHomework1

---
+++ src/test/java/GitHubHomework1.java
@@ -0,0 +1,24 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubHomework1 {
+    @Test
+    void successfulSearchTest() {
+        //Откройте страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+        //Перейдите в раздел Wiki проекта
+        $("[id=wiki-tab]").click();
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("[id=wiki-pages-box] button").click();
+        $("[id=wiki-pages-box]").shouldHave(text("SoftAssertions"));
+        //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $("[id=wiki-pages-box]").$(byText("SoftAssertions")).click();
+        $("[class=markdown-body]").shouldHave(text("Using JUnit5 extend test class"));
+
+    }
+}
